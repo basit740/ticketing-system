9ccc7c5f910d9106d2ddeb00040f9a19dd1939f9
Basit Ali <alib4111@gmail.com>
2023-09-13 01:54:14 +0500

    seed function

---
+++ backend/seedEmp.js
@@ -0,0 +1,29 @@
+const { Employee } = require('../db/index.js');
+
+const seedEmp = async () => {
+	try {
+		await Employee.bulkCreate([
+			{
+				firstName: 'John',
+				lastName: 'Doe',
+				birthDate: '01/01/1995',
+				employeeNumber: 1,
+				department: 'JavaScript',
+			},
+			{
+				firstName: 'Jane',
+				lastName: 'Smith',
+				birthDate: '01/01/1995',
+				employeeNumber: 2,
+				department: 'HTML',
+			},
+			// Add more users as needed
+		]);
+
+		console.log('Users seeded successfully');
+	} catch (error) {
+		console.error('Failed to seed users:', error);
+	}
+};
+
+module.exports = seedEmp;
